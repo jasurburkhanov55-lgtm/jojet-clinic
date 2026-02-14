db0d4e331e7e606a7745888e0ad5468821c0cad5
jasurburkhanov55-lgtm <jasurburkhanov55@gmail.com>
2026-02-14 23:51:49 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
-
+export default defineConfig({
+  plugins: [react()],
+  base: '/jojet-clinic/',  // 
+})
 // https://vite.dev/config/
 export default defineConfig({
   base: '/jojet-clinic/',
